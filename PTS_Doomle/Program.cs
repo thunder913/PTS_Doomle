a94b20b1b4be43be8395cb9f4d9fd2057b7c14fb
thunder913 <andon_00@abv.bg>
2022-04-22 20:03:24 +0300

    removed front end files and published to azure

--- PTS_Doomle/Program.cs
+++ PTS_Doomle/Program.cs
@@ -6,31 +6,26 @@ using PTS_Doomle.Response.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<ICalculationManager, CalculationManager>();
 builder.Services.AddTransient<IDataManager, DataManager>();
 builder.Services.AddTransient<IExcelDataReader, PTS_Doomle.Data.ExcelDataReader>();
 builder.Services.AddTransient<IDataSerializationManager, DataSerializationManager>();
 builder.Services.AddTransient<IResponseManager, ResponseManager>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
+app.UseStaticFiles();
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseStaticFiles();
-app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html"); ;
 app.Run();
